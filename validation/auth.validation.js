3aa62b316357e7a29e255652b1ac7ad8725c6708
vasanthan231020 <131371162+vasanthan231020@users.noreply.github.com>
2023-07-07 16:45:25 +0530

    Add files via upload

--- validation/auth.validation.js
+++ validation/auth.validation.js
@@ -1,1 +1,20 @@
+const joi = require('joi');
 
+const signupValidation = async (req, res, next) => {
+	try {
+		let schema = joi.object({
+			email: joi.string().min(7).max(30).email().required(),
+			username: joi.string().required(),
+			password: joi.string().min(8).max(20).required()
+		})
+		await schema.validateAsync({...req.body, ...req.query, ...req.params});
+		next();
+	} catch (err) {
+		res.status(500).json({
+			status: 500,
+			message: err.details[0].message
+		})
+	}
+}
+
+module.exports = signupValidation;
